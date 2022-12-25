191839f1faee717ec24810c29af20600a2ddbef1
clockworkcoding <yodadamax@gmail.com>
2022-12-25 04:22:53 -0500

    good progress

--- index.js
+++ index.js
@@ -20,82 +20,205 @@ function preview_csv(e) {
         complete: function(result) {
             if (result.data && result.data.length > 0) {
                 var grTitles = groupBy(result.data, 'isbn');
-                var sgTitles = grTitles.map(transformGrData);
-                initDataTable(result.data)
+                var sgTitles = transformGrData(grTitles);
+                initDataTable(sgTitles)
             }
         }
     });
 }
 
-function transformGrData(title) {
-    return {
-        title : title[0].title
-    }
+function transformGrData(grTitles) {
+    let sgTitles = [];
+    grTitles.forEach(function(t, key){
+        t = t.sort((a,b)=> Date.parse(a.timestamp) - Date.parse(b.timestamp));
+        let lastActivity = new Date(t[0].timestamp).toISOString().split('T')[0]
+        //let dateAdded = new Date(t[t.length - 1].timestamp).toISOString().split('T')[0]
+
+        let borrowed = t.filter(a => a.activity == "Borrowed").sort((a,b)=> Date.parse(b.timestamp) - Date.parse(a.timestamp))[0];
+        let returned = t.filter(a => a.activity == "Returned").sort((a,b)=> Date.parse(a.timestamp) - Date.parse(b.timestamp))[0];
+        //let held = t.filter(a => a.Activity == "Placed on hold").sort((a,b)=> Date.parse(a.timestamp) - Date.parse(b.timestamp))[0];
+        let dateRead = null;
+        let readCount = 0;
+        let shelf = "to-read";
+        if (returned != null){
+            shelf = "read";
+            readCount =1;
+            dateRead = new Date(returned.timestamp).toISOString().split('T')[0];
+        }
+        else if (borrowed != null){
+            shelf = "currently-reading";
+        }
+        let minDate = Math.min(... t.filter(x => x.timestamp != '').map((a) => Date.parse(a.timestamp)));
+        let dateAdded = new Date();
+        dateAdded.setTime(minDate);
+
+        sgTitles.push({
+            lastActivity: lastActivity,
+            cover:t[0].cover,
+            bookId:0,
+            title:t[0].title,
+            author:t[0].author,
+            authorLF:null,
+            additionalAuthor:null,
+            isbn:0,
+            isbn13:t[0].isbn,
+            myRating: null,
+            averageRating:null,
+            publisher:null,
+            binding:null,
+            numberOfPages: null,
+            yearPublished:null,
+            originalPublicationYear:null,
+            dateRead: dateRead,
+            dateAdded: dateAdded.toISOString().split('T')[0],
+            bookshelves:null,
+            bookshelvesWithPositions:null,
+            exclusiveShelf: shelf,
+            myReview:null,
+            spoiler:null,
+            privateNotes:null,
+            readCount: readCount,
+            ownedCopies:0
+        });
+    })
+    return sgTitles;
 }
-function htmlTableGenerator(content) {
-    let csv_preview = document.getElementById('csv-preview');
-
-    let html = '<table id="example" class="table table-condensed table-hover table-striped" style="width:100%">';
-
-    if (content.length == 0 || typeof(content[0]) === 'undefined') {
-        return null
-    } else {
-        const header = content[0];
-        const data = content.slice(1);
-
-        html += '<thead>';
-        html += '<tr>';
-        header.forEach(function(colData) {
-            html += '<th>' + colData + '</th>';
-        });
-        html += '</tr>';
-        html += '</thead>';
-        html += '<tbody>';
-
-        data.forEach(function(row) {
-            if (header.length === row.length) {
-                html += '<tr>';
-                row.forEach(function(colData) {
-                    html += '<td>' + colData + '</td>';
-                });
-                html += '</tr>';
-            }
-        });
-
-        html += '</tbody>';
-        html += '</table>';
-
-        // insert table element into csv preview
-        csv_preview.innerHTML = html;
-
-        // initialise DataTable
-        initDataTable();
-    }
+function isValidDate(d) {
+    return d instanceof Date && !isNaN(d);
 }
 
 function initDataTable(content) {
     $('#example').dataTable({
         data: content,
-            columns: [{ data: 'cover' }, { data: 'title' }, { data: 'author' }, { data: 'publisher' }, { data: 'isbn' }, { data: 'timestamp' }, { data: 'activity' }, { data: 'details' }, { data: 'library' }],
+            columns: [
+                {
+                    data: 'lastActivity',
+                    type: 'date'
+                },
+                {
+                    data: 'cover',
+                    render: function(data) {
+                        return '<img src="'+data+'"  height="128">'
+                    }
+                },
+                {
+                    data: 'bookId',
+                    visible: false
+                },
+                {
+                    data: 'title'
+                },
+                {
+                    data: 'author'
+                },
+                {
+                    data: 'authorLF',
+                    visible: false
+                },
+                {
+                    data: 'additionalAuthor',
+                    visible: false
+                },
+                {
+                    data: 'isbn',
+                    visible: false
+                },
+                {
+                    data: 'isbn13'
+                },
+                {
+                    data: 'myRating'
+                },
+                {
+                    data: 'averageRating',
+                    visible: false
+                },
+                {
+                    data: 'publisher',
+                    visible: false
+                },
+                {
+                    data: 'binding',
+                    visible: false
+                },
+                {
+                    data: 'numberOfPages',
+                    visible: false
+                },
+                {
+                    data: 'yearPublished',
+                    visible: false
+                },
+                {
+                    data: 'originalPublicationYear',
+                    visible: false
+                },
+                {
+                    data: 'dateRead',
+                    type: 'date'
+                },
+                {
+                    data: 'dateAdded',
+                    type: 'date'
+                },
+                {
+                    data: 'bookshelves',
+                    visible: false
+                },
+                {
+                    data: 'bookshelvesWithPositions',
+                    visible: false
+                },
+                {
+                    data: 'exclusiveShelf'
+                },
+                {
+                    data: 'myReview'
+                },
+                {
+                    data: 'spoiler',
+                    visible: false
+                },
+                {
+                    data: 'privateNotes',
+                    visible: false
+                },
+                {
+                    data: 'readCount'
+                },
+                {
+                    data: 'ownedCopies',
+                    visible: false
+                },
+            ],
+        order:[[0,'desc']],
         scrollX: true,
         scrollY: (window.innerHeight / 2) + "px",
         dom: 'Bfrtip',
         buttons: [
-            'colvis',
             {
                 extend: 'csv',
                 text: 'Download CSV',
                 exportOptions: {
-                    columns: ':visible'
+                    columns:[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
                 }
             }
         ]
     })
 }
 
-var groupBy = function(xs, key) {
-  return xs.reduce(function(rv, x) {
-    (rv[x[key]] = rv[x[key]] || []).push(x);
-    return rv;
-  }, {});
-};
+function groupBy(items, key) {
+    // initialize our map
+    const map = new Map();
+    items.forEach((item) => {
+      // get the value we're grouping by
+      const keyValue = item[key];
+      // get the array of items for this key value. default to an empty array
+      const currArr = map.has(keyValue) ? map.get(keyValue) : [];
+      // add the current item
+      currArr.push(item);
+      // update the array
+      map.set(keyValue, currArr);
+    });
+    return map;
+  }
